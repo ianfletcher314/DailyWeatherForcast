7873d894225311d6567972c285e057b7bb7e6b8b
Ian Fletcher <ianfletcher314@gmail.com>
2021-03-15 17:01:58 -0500

    trying to add everyting new to the repo. Had issues committing so trying again

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,6 @@
 // on load function 1 that gets moment time 
-window.onload = function startPage(){
-    var dayAndTime = moment().format('LL'); 
+window.onload = function startPage() {
+    var dayAndTime = moment().format('LL');
     console.log(dayAndTime)
 }
 
@@ -8,141 +8,183 @@ window.onload = function startPage(){
 
 
 // event listenerfor button that that inputs location. function it calls below
-document.getElementById("locationButton").addEventListener("click", function(event){
+document.getElementById("locationButton").addEventListener("click", function (event) {
     event.preventDefault()
     // variables section for click function
     var searchInput = document.getElementById("textArea");
     var location = searchInput.value.trim()
-    var inputSectionElement = document.getElementById("container")
-    // var temperature = ""
-    // var humidity = ""
-    // var windSpeed = ""
-    // var ultraViolet = ""
-    // api url broken into sections with the location element set as changing variable
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + location + '&units=imperial&appid=e52b27c3a12708b53090d96de3562d01';
-    console.log("Event Listener is firing")
-    console.log(searchInput.value)
-    inputSectionElement.classList.add('hide')
+    getWeatherForCity(location)
     
+    var nameFromLocal = []
+    if (localStorage.getItem('names')) {
+    	nameFromLocal = JSON.parse(localStorage.getItem('names'))
+    }
+    nameFromLocal.push(location)
+    console.log(nameFromLocal)
+	localStorage.setItem('names', JSON.stringify(nameFromLocal))
+    var searchHolder = document.getElementById("searchDiv")
+    var searchContainer = document.createElement("div")
+    searchHolder.appendChild(searchContainer)
+    
+    for (let i = 0; i < nameFromLocal.length; i++) {
+        var pastSearchesElement = document.createElement("button")
+        pastSearchesElement.classList.add('btn-success')
+        pastSearchesElement.classList.add('btn')
+        pastSearchesElement.classList.add('grab')
+
+        pastSearchesElement.innerHTML = nameFromLocal[i]
+        searchHolder.appendChild(pastSearchesElement)
+    }
+    // pastSearchesElement.textContent = `Past Searches: ${nameFromLocal}`
+    // searchContainer.appendChild(pastSearchesElement)
+
+    // here I need a looping function that takes the array of selected elements and makes an event listener for each individual
+    // element  it that when clickedc alls the getWeatherForCity function using the innerHTML of the element as the parameter
+    var buttonElements = document.querySelectorAll(".grab")
+    for (let i = 0; i < buttonElements.length; i++) {
+        console.log(buttonElements[i])
+        // WHY DOENT THIS ONLY WORK ON CLICK???? IT RUNS EVERY TIME THE PAGE LOADS
+        // buttonElements[i].addEventListener("click", getWeatherForCity(buttonElements[i].innerHTML))
+        
+    }
+    
+
+});
+
+
+
+function getWeatherForCity(city) {
+
+    var inputSectionElement = document.getElementById("container")
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=e52b27c3a12708b53090d96de3562d01';
+    console.log("Event Listener is firing")
+    // console.log(searchInput.value)
+    inputSectionElement.classList.add('hide')
+
     // grab the information from the API using the location provided by user
-    // ????? HOW DO YOU SAVE DATA OBJECT TO VARIABLES I CAN ONLY GET IT TO CONSOLE LOG?????
     fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        var temperature = data.main.temp_max
-        var humidity = data.main.humidity
-        var windSpeed = data.wind.speed
-        var weatherIcon = data.weather[0].icon
-        var cityLatitude = data.coord.lat
-        var cityLongitude = data.coord.lon
-        var weatherIconPhoto = "http://openweathermap.org/img/wn/" + weatherIcon +"@2x.png"
-        console.log(temperature,"Degrees is the Temperature")
-        console.log(humidity, "% Humidity")
-        console.log(windSpeed,"mph Wind Speed")
-        console.log(cityLatitude)
-        console.log(cityLongitude)
-
-
-
-        // create new element that house the data we are trying to pull from the API. weatherHolder will be the main container
-        var weatherHolder = document.getElementById("weatherDiv")
-        var weatherContainer = document.createElement("div")
-        weatherHolder.appendChild(weatherContainer)
-        console.log(weatherHolder)
-
-        // create element that will hold city's name
-        var cityHeader = document.createElement("div")
-        cityHeader.classList.add("cityHeader")
-        cityHeader.textContent = location
-        weatherHolder.appendChild(cityHeader)
-        console.log(cityHeader)
-        console.log(weatherHolder)
-
-        // create element that holds list of city's weather
-        var cityWeatherList = document.createElement("ul")
-        cityWeatherList.classList.add("weatherList")
-        var cityTemperature = document.createElement("li")
-        cityTemperature.textContent = temperature
-        console.log(cityTemperature)
-        cityWeatherList.appendChild(cityTemperature)
-        var cityHumidity = document.createElement("li")
-        cityHumidity.textContent = humidity
-        console.log(cityHumidity)
-        cityWeatherList.appendChild(cityHumidity)
-        var cityWind = document.createElement("li")
-        cityWind.textContent = windSpeed
-        console.log(cityWind)
-        cityWeatherList.appendChild(cityWind)
-        var cityTime = document.createElement("h1")
-        cityTime.textContent = moment().format('ll');  
-        console.log(cityTime)
-        weatherHolder.appendChild(cityTime)
-        var cityWeatherIcon= document.createElement("img")
-        cityWeatherIcon.src = weatherIconPhoto
-        console.log(cityWeatherIcon)
-        weatherHolder.appendChild(cityWeatherIcon)
-         // function that takes lat and lon and gets UV from the other API
-        var oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+cityLatitude+"&lon="+cityLongitude+"&units=imperial&appid=e52b27c3a12708b53090d96de3562d01"
-        fetch(oneCallUrl)
         .then(response => response.json())
         .then(data => {
-        console.log(data)
-        var cityUv = document.createElement("li")
-        cityUv.textContent = data.current.uvi
-        cityWeatherList.appendChild(cityUv)
-        weatherHolder.appendChild(cityWeatherList)
-        console.log(weatherHolder)
+            console.log(data)
+            var temperature = data.main.temp_max
+            var humidity = data.main.humidity
+            var windSpeed = data.wind.speed
+            var weatherIcon = data.weather[0].icon
+            var cityLatitude = data.coord.lat
+            var cityLongitude = data.coord.lon
+            var weatherIconPhoto = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png"
+            console.log(temperature, "Degrees is the Temperature")
+            console.log(humidity, "% Humidity")
+            console.log(windSpeed, "mph Wind Speed")
+            console.log(cityLatitude)
+            console.log(cityLongitude)
 
 
 
-        var loopWeatherList = document.createElement("ul")
-        loopWeatherList.classList.add("weatherList")
-        // create elements for future 5 days 
-        for (let i = 0; i < data.daily.length - 2; i++) {
-            var loopCityTime = document.createElement("h1")
-            loopCityTime.textContent = moment().add(i + 1, 'days').calendar();  
-            console.log(loopCityTime)
-            loopWeatherList.appendChild(loopCityTime)
+            // create new element that house the data we are trying to pull from the API. weatherHolder will be the main container
+            var weatherHolder = document.getElementById("weatherDiv")
+            var weatherContainer = document.createElement("div")
+            weatherHolder.appendChild(weatherContainer)
+            console.log(weatherHolder)
 
-            var loopIconElement = document.createElement("img")
-            var loopIconId = data.daily[i].weather[0].icon
-            var loopIconPhoto = "http://openweathermap.org/img/wn/" + loopIconId +"@2x.png"
-            loopIconElement.src = loopIconPhoto
-            loopWeatherList.appendChild(loopIconElement)
+            // create element that will hold city's name
+            var cityHeader = document.createElement("div")
+            cityHeader.classList.add("cityHeader")
+            cityHeader.textContent = city
+            weatherHolder.appendChild(cityHeader)
+            console.log(cityHeader)
+            console.log(weatherHolder)
 
-            var loopTempElement = document.createElement("li")
-            var loopTemp = data.daily[i].temp.day
-            loopTempElement.textContent= loopTemp 
-            loopWeatherList.appendChild(loopTempElement)
+            // create element that holds list of city's weather
+            var cityWeatherList = document.createElement("ul")
+            cityWeatherList.classList.add("weatherList")
+            var cityTemperature = document.createElement("li")
+            cityTemperature.textContent = `${temperature} Degrees Farenheit`
+            console.log(cityTemperature)
+            cityWeatherList.appendChild(cityTemperature)
+            var cityHumidity = document.createElement("li")
+            cityHumidity.textContent = `${humidity} Percent Humidity`
+            console.log(cityHumidity)
+            cityWeatherList.appendChild(cityHumidity)
+            var cityWind = document.createElement("li")
+            cityWind.textContent = `${windSpeed} MPH Wind Speed`
+            console.log(cityWind)
+            cityWeatherList.appendChild(cityWind)
+            var cityTime = document.createElement("h1")
+            cityTime.textContent = moment().format('ll');
+            console.log(cityTime)
+            weatherHolder.appendChild(cityTime)
+            var cityWeatherIcon = document.createElement("img")
+            cityWeatherIcon.src = weatherIconPhoto
+            console.log(cityWeatherIcon)
+            weatherHolder.appendChild(cityWeatherIcon)
+            // function that takes lat and lon and gets UV from the other API
+            var oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLatitude + "&lon=" + cityLongitude + "&units=imperial&appid=e52b27c3a12708b53090d96de3562d01"
+            fetch(oneCallUrl)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    var cityUv = document.createElement("li")
+                    cityUv.textContent = data.current.uvi
+                    // change the text color to the UVI index color to indicate severity 
+                    if(cityUv.textContent < 3) {
+                        cityUv.classList.add("green")
+                    }
+                    else if(3 < cityUv.textContent < 6) {
+                        cityUv.classList.add("yellow")
+                    }
+                    else if(6 < cityUv.textContent < 8) {
+                        cityUv.classList.add("orange")
+                    }
+                    else if(8 < cityUv.textContent < 11) {
+                        cityUv.classList.add("red")
+                    }
+                    else if(11 < cityUv.textContent) {
+                        cityUv.classList.add("violet")
+                    }
+                    cityWeatherList.appendChild(cityUv)
+                    weatherHolder.appendChild(cityWeatherList)
+                    console.log(weatherHolder)
 
-            var loopHumidElement = document.createElement("li")
-            var loopHumid = data.daily[i].humidity
-            loopHumidElement.textContent= loopHumid
-            loopWeatherList.appendChild(loopHumidElement)
 
 
-            
-            
-            
-           
-            
-        }
-        weatherHolder.appendChild(loopWeatherList)
+                    var loopWeatherList = document.createElement("ul")
+                    loopWeatherList.classList.add("weatherList")
+                    // create elements for future 5 days 
+                    for (let i = 0; i < data.daily.length - 2; i++) {
+                        var loopCityTime = document.createElement("h1")
+                        loopCityTime.textContent = moment().add(i + 1, 'days').calendar();
+                        console.log(loopCityTime)
+                        loopWeatherList.appendChild(loopCityTime)
+
+                        var loopIconElement = document.createElement("img")
+                        var loopIconId = data.daily[i].weather[0].icon
+                        var loopIconPhoto = "http://openweathermap.org/img/wn/" + loopIconId + "@2x.png"
+                        loopIconElement.src = loopIconPhoto
+                        loopWeatherList.appendChild(loopIconElement)
+
+                        var loopTempElement = document.createElement("li")
+                        var loopTemp = data.daily[i].temp.day
+                        loopTempElement.textContent = `${loopTemp} Degrees Farenheit`
+                        loopWeatherList.appendChild(loopTempElement)
+
+                        var loopHumidElement = document.createElement("li")
+                        var loopHumid = data.daily[i].humidity
+                        loopHumidElement.textContent = `${loopHumid} % Humidity`
+                        loopWeatherList.appendChild(loopHumidElement)
+
+
+
+
+
+
+
+                    }
+                    weatherHolder.appendChild(loopWeatherList)
+
+                })
+
+
 
         })
-
-        
-        
-    })          
-   
-    
-
-
-
-    // function 2that puts the location they inputed into the middle of the API URL so we can get the specific information for that location
-
-    // function 3 that creates the elements that display the results of function 2 ie the columns with temp humidity icon etc.
-
-
-  });
+}
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # DailyWeatherForcast
+
+## The Repository 
+This Repository is where Ian Fletcher's Daily Weather Forcast Application is Housed 
+
+## Installation
+In order to acess and refractor the code you must first go through the Installation Process.
+
+1) Open GitHub and navigate to the main repository 
+
+2) Click the green "Code" button Above the top right corner of the files 
+
+3) To clone the repository using HTTPS, under "Clone with HTTPS", click the clipboard icon. To clone the repository using an SSH key, including a certificate issued by your organization's SSH certificate authority, click "Use SSH", then click the clipboard icon. To clone a repository using GitHub CLI, click "Use GitHub CLI", then click the clipboard icon.
+
+4) Open Terminal and navigate to the directory where you want the repository to be 
+
+5) Type "git clone ", and then paste the SSH, CLI, or HTTPS link you coppied on GitHub and press ENTER 
+
+This should put a copy of the repository into the directory you had chosen in step 1
+
+## Usage
+This application should allow users to input the name of a city and recieve the local weather in that city. Once searched the application displays an Icon depicting the weather along with temperature, humidty, wind speed,  and UV index. Underneath Todays forcast the user will be presented with a 5 day forcast that includes weather icon, temperature, and humidity. The search history of the user is presented in the form of buttons at the bottom. When the button is clicked the user is presented with the weather from the selected city just as if they had searched it. 
+
+
+![alt text](Assets/images/weather2.png)
+![alt text](Assets/images/weather1.png)
+
+[Check Out The Full Page](https://ianfletcher314.github.io/DailyWeatherForcast/)
+
+## Credits
+
+- [GitHub's Document on Cloning Repositories (used in Instalation section)](https://docs.github.com/en/github/creating-cloning-and-archiving-repositories/cloning-a-repository) 
+- [VS Code's ReadMe Page (used for formating and licensing syntax)](https://github.com/microsoft/vscode/blob/master/README.md)
+
+## License 
+
+Copyright (c) Horision SSS Inc. All Rights Reserved.
+
+Licensed under the [MIT](license.txt) license
